35b07a01cd231c855a1b81c9a17ecec8917f1290
NicolasFue05 <nicolasjfc05@gmail.com>
2025-02-17 21:11:06 -0500

    feat: :sparkles: fetchData imported and added to the code, getTime imported and added to the code: fetchData is used to send the requests, setInterval is used to fetch data every time, the getTime function is used to know the time

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { MainMenu, StartMenu } from './Menu/menu.js'
+import fetchData from './Processor/fetchingData.js'
+import getTime from './Processor/time.js'
 
 async function run() {
   const mainMenuResult = await MainMenu()
@@ -7,7 +9,16 @@ async function run() {
   // If the mainresult is start show the startMenu
   if (mainMenuResult === 'Start') {
     const startMenuResult = await StartMenu()
-    console.log(startMenuResult)
+
+    console.log('Fetching Data every...', startMenuResult.timeInput)
+    await fetchData(startMenuResult.url, startMenuResult.endpoint, startMenuResult.endpointInput, startMenuResult.timeInput, startMenuResult.jsonPath)
+
+    // Schedule repeated fetching
+    setInterval(() => {
+      fetchData(startMenuResult.url, startMenuResult.endpointInput, startMenuResult.timeInput, startMenuResult.jsonPath)
+    }, getTime(startMenuResult.timeInput))
+
+
   } else if (mainMenuResult === 'Exit') {
     console.log('Tank\'s for using POSTY!')
   }
