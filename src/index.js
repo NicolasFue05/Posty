3b46b04526fca0f12586116e4b586d4e5c2556b1
NicolasFue05 <nicolasjfc05@gmail.com>
2025-02-18 04:18:23 -0500

    fix: :bug: SetInterval removed because the logic has changed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import { MainMenu, StartMenu } from './Menu/menu.js'
 import fetchData from './Processor/fetchingData.js'
-import getTime from './Processor/time.js'
 
 async function run() {
   const mainMenuResult = await MainMenu()
   console.log(mainMenuResult)
 
-  // If the mainresult is start show the startMenu
   if (mainMenuResult === 'Start') {
     const startMenuResult = await StartMenu()
+    console.log('Fetching Data...')
 
-    console.log('Fetching Data every...', startMenuResult.timeInput)
-    await fetchData(startMenuResult.url, startMenuResult.endpoint, startMenuResult.endpointInput, startMenuResult.timeInput, startMenuResult.jsonPath)
-
-    // Schedule repeated fetching
-    setInterval(() => {
-      fetchData(startMenuResult.url, startMenuResult.endpointInput, startMenuResult.timeInput, startMenuResult.jsonPath)
-    }, getTime(startMenuResult.timeInput))
-
+    await fetchData(
+      startMenuResult.url,
+      startMenuResult.endpointInput,
+      startMenuResult.jsonPath
+    )
 
   } else if (mainMenuResult === 'Exit') {
-    console.log('Tank\'s for using POSTY!')
+    console.log('Thanks for using POSTY!')
   }
 }
+
 
 run()
