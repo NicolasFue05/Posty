39363305204935510f2d892b5af1a792b4313c7e
NicolasFue05 <nicolasjfc05@gmail.com>
2025-02-24 19:40:39 -0500

    fix: :bug: SettingsMenu exported and used successfully | New try catch statement added successfully | New checks added in the startMenuResult data | Catching ctrl + c input with the exitMessage

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
-import { MainMenu, StartMenu } from './Menu/menu.js'
+import { MainMenu, SettingsMenu, StartMenu } from './Menu/menu.js'
+import { exitMessage } from './Menu/title.js'
 import fetchData from './Processor/fetchingData.js'
 import { addData, createLocalJson } from './Processor/jsonFile.js'
+import { checkJsonPath } from './Processor/paths.js'
 
 async function run() {
-  const mainMenuResult = await MainMenu()
-  console.log(mainMenuResult)
+  try {
+    const mainMenuResult = await MainMenu()
+    console.log(mainMenuResult)
 
-  if (mainMenuResult === 'Start') {
-    const startMenuResult = await StartMenu()
-    console.log('Fetching Data...')
+    if (mainMenuResult === 'Start') {
+      const startMenuResult = await StartMenu()
 
-    const data = await fetchData(
-      startMenuResult.url,
-      startMenuResult.endpointInput,
-      startMenuResult.jsonPath
-    )
+      const data = await fetchData(
+        startMenuResult.url,
+        startMenuResult.endpointInput,
+        startMenuResult.jsonPath
+      )
 
-    if (startMenuResult.jsonChoice === 'Create a local Json File') {
-      createLocalJson(data)
-    } else {
-      addData(startMenuResult.jsonPath, data)
+      // if fetchData is null return a error message
+      if (data === null) {
+        return null
+      }
+
+      if (startMenuResult.jsonChoice === 'Create a local Json File') {
+        createLocalJson(data)
+      } else if (startMenuResult.jsonChoice === 'Give a path to the Json File') {
+        addData(startMenuResult.jsonPath, data)
+      } else {
+        const jsonPath = await checkJsonPath()
+        addData(jsonPath, data)
+      }
+
+    } else if (mainMenuResult === 'Exit') {
+      exitMessage()
+    } else if(mainMenuResult === 'Settings') {
+      await SettingsMenu()
     }
-
-  } else if (mainMenuResult === 'Exit') {
-    console.log('Thanks for using POSTY!')
+  } catch (e) {
+    if (e instanceof Error && e.name === 'ExitPromptError') {
+      exitMessage()
+    } else {
+      throw e
+    }
   }
 }
 
