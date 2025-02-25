ae782e25f55f3f3c7bfed4174cc807f7334eb597
NicolasFue05 <nicolasjfc05@gmail.com>
2025-02-24 19:08:24 -0500

    feat: :sparkles: Refactored Code using prettier | node:process, saveJsonPath and checkJsonPath  functions imported correctly | Use instructions removed from mainMenu choices | jsonPath checking changed | SettingsMenu function added successfully creating the menu with 2 options, instructions needed for the second option

--- src/Menu/menu.js
+++ src/Menu/menu.js
@@ -1,5 +1,7 @@
 import inquirer from 'inquirer'
 import ASCIITitle from './title.js'
+import process from 'node:process'
+import { saveJsonPath, checkJsonPath } from '../Processor/paths.js'
 
 const MainMenu = async () => {
   console.clear()
@@ -9,14 +11,10 @@ const MainMenu = async () => {
     {
       type: 'list',
       name: 'main_choice',
-      message: 'WELCOME TO POSTY!\n Use The Arrow Keys or The Vim Keys (J-K) To Select The Options',
-      choices: [
-        'Start', 
-        'Use Instructions',
-        'Settings',
-        'Exit'
-      ]
-    }
+      message:
+        'WELCOME TO POSTY!\n Use The Arrow Keys or The Vim Keys (J-K) To Select The Options',
+      choices: ['Start', 'Settings', 'Exit'],
+    },
   ])
 
   return answers.main_choice
@@ -33,7 +31,7 @@ const StartMenu = async () => {
       type: 'input',
       name: 'start_input',
       message: 'Set URL',
-    }
+    },
   ])
 
   // Endpoint Select
@@ -42,62 +40,90 @@ const StartMenu = async () => {
       type: 'list',
       name: 'endpoint_select',
       message: 'Endpoint?',
-      choices: [
-        'No required',
-        'Set Endpoint'
-      ]
-    }
+      choices: ['No required', 'Set Endpoint'],
+    },
   ])
 
-  let endpointInput = null   
+  let endpointInput = null
 
   if (endpointAnswer.endpoint_select === 'Set Endpoint') {
     endpointInput = await inquirer.prompt([
       {
         type: 'input',
         name: 'endpoint_input',
-        message: 'Put the EndPoint'
-      }
+        message: 'Put the EndPoint',
+      },
     ])
-  } 
-
-
+  }
+  // add the checkJsonPath function
+  // if chechJsonPath is null show the normal prompt, if it not, show the select saved json and return the firstline of the saved txt
   const jsonChoice = await inquirer.prompt([
     {
       type: 'list',
       name: 'json_input',
       message: 'Json Location',
       choices: [
+        'Use Saved Json Path',
         'Create a local Json File',
-        'Give a path to the Json File'
-      ]
-    }
+        'Give a path to the Json File',
+      ],
+    },
   ])
   // if user want to create a local json file, ask for a path or create one in the Data folder
   let jsonPath = null
 
-  if (jsonChoice.json_input !== 'Create a local Json File') {
+  if (jsonChoice.json_input === 'Give a path to the Json File') {
     jsonPath = await inquirer.prompt([
       {
         type: 'input',
         name: 'json_path',
         message: 'Please write the path to the json file:',
-      }
+      },
     ])
+  } else if (jsonChoice.json_input === 'Use Saved Json Path') {
+    jsonPath = await checkJsonPath()
   }
 
-  return ({
+  return {
     url: inputAnswers.start_input,
     endpoint: endpointAnswer.endpoint_select,
     endpointInput: endpointInput?.endpoint_input || '',
     jsonChoice: jsonChoice.json_input,
-    jsonPath: jsonPath?.json_path || 'No required'
-  })
+    jsonPath: jsonPath?.json_path,
+  }
 }
 
-// TODO: Create the instructions component
+const SettingsMenu = async () => {
+  console.clear()
+  console.log(ASCIITitle())
 
-export {
-  MainMenu,
-  StartMenu
-} 
+  // Create the save current json path option
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'config_menu',
+      message: 'SETTINGS',
+      choices: ['Save Current Json Path', 'Instructions'],
+    },
+  ])
+
+  let jsonPath = null
+  if (answers.config_menu === 'Save Current Json Path') {
+    jsonPath = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'json_path',
+        message: 'Please Put The Json File Path',
+        default: process.cwd(),
+      },
+    ])
+
+    saveJsonPath(String(jsonPath.json_path))
+  }
+
+  if (answers.config_menu === 'Instructions') {
+    console.log('INSTRUCTIONS!!')
+  }
+}
+
+export { MainMenu, StartMenu, SettingsMenu }
