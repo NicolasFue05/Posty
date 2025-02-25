b648f61691d7d34cd86a4401d3abc82c79a56c8a
NicolasFue05 <nicolasjfc05@gmail.com>
2025-02-24 19:36:45 -0500

    refactor: :recycle: Refactored Code | New fetching data log added | Catching invalid URL error function added successfully | Default export changed

--- src/Processor/fetchingData.js
+++ src/Processor/fetchingData.js
@@ -1,9 +1,5 @@
-// Create a function that receive the URL, the Enpoint, the time and the json file and fetch the API
-
-
 // Each minute, ask for the new endpoint, if theres no endpoint, send request in the same URL
-async function fetchData(URL, endpoint = '') {
-
+export default async function fetchData(URL, endpoint = '') {
   try {
     const response = await fetch(`${URL}${endpoint}`)
 
@@ -11,15 +7,15 @@ async function fetchData(URL, endpoint = '') {
       throw new Error(`ERROR! \n Status: ${response.status}`)
     }
 
+    console.log('✔ Fetching Data...')
     const data = await response.json()
     console.log('✔ Data fetched correctly')
-  
+
     return data
-
   } catch (e) {
-    console.error(`Fetch Error: ${e.message}`)
+    if (e instanceof TypeError) {
+      console.error(`❌ THE GIVEN API IS NOT AVAILABLE | ERROR: ${e.message}`)
+    }
     return null
-  }  
+  }
 }
-
-export default fetchData
