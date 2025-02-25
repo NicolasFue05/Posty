88cb6209c261c7f118cee89b5063cb20aa948d2e
NicolasFue05 <nicolasjfc05@gmail.com>
2025-02-24 19:32:17 -0500

    refactor: :recycle: Refactored code | Add new emojis to the console logs

--- src/Processor/jsonFile.js
+++ src/Processor/jsonFile.js
@@ -7,16 +7,15 @@ const LOCAL_FILE_PATH = path.join(DATA_PATH, FILE_NAME)
 
 export function createLocalJson(data) {
   try {
-    console.log(` Checking if folder exists: ${DATA_PATH}`)
+    console.log(`✔ Checking if folder exists: ${DATA_PATH}`)
 
     // Ensure the Data folder exists
     if (!fs.existsSync(DATA_PATH)) {
-      console.log('Folder does NOT exist, creating it...')
+      console.log('❌ Folder does NOT exist, ✔ Creating it...')
       fs.mkdirSync(DATA_PATH, { recursive: true })
     } else {
-      console.log('Folder already exists.')
+      console.log('✔ Folder already exists.')
     }
-
 
     // Write data to the file
     fs.writeFileSync(LOCAL_FILE_PATH, JSON.stringify(data, null, 2))
@@ -27,6 +26,7 @@ export function createLocalJson(data) {
   }
 }
 
+// Change the function to overwrite the json file
 export function addData(filePath, data) {
   try {
     // Extract directory path (so we don't treat a file as a folder)
@@ -34,8 +34,8 @@ export function addData(filePath, data) {
 
     // Ensure the directory exists
     if (!fs.existsSync(dirPath)) {
-      console.log('Directory does not exist. Creating it...')
-      fs.mkdirSync(dirPath, { recursive: true }) // Create the directory
+      console.error('❌  DIRECTORY DOES NOT EXIST')
+      return
     }
 
     // Overwrite the file with new data
